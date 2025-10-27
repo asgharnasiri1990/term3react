1e7423398b638eb194268dcf1a25495e99da9200
asghar <asgharnasiri1990@gmail.com>
2025-10-27 23:43:52 +0330

    first react exercise

---
+++ react1/my-first-react-app/src/App.jsx
@@ -0,0 +1,49 @@
+const Card = ({title}) => {
+  return(
+    <div>
+      <h2>{title}</h2>
+    </div>
+  )
+}
+
+const App = () => {
+  return(
+    <div className="card-container">
+
+    <h2>Functional arrow app is written here:</h2>
+
+    <Card title='Star wars' rating={5} iscool={true} actors={[{name:'actors'}]} />  
+    <Card title='Avatar'/>  
+    <Card title='The lion king'/>  
+    
+ 
+  </div>
+  )
+  
+  
+}
+
+
+/*
+const Card = ({title}) =>{
+  return(
+    <div>
+      <h2>{title} </h2>
+    </div>
+    
+  )
+}
+
+
+const App = () =>{
+  return(
+  <div>
+    <h2>Hello from a new exercises:</h2>
+
+    <Card title='New star wars' />
+     <Card title='New Avatar'/>
+     <Card title='new the lion king' />
+   </div>
+   )
+ } */
+export default App
