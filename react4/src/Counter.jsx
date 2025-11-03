bcf28e91ba51932bbc825138d2ca5961608bef79
asghar <asgharnasiri1990@gmail.com>
2025-11-03 12:33:30 +0330

    css

--- react4/src/Counter.jsx
+++ react4/src/Counter.jsx
@@ -1,44 +1,44 @@
 import { useState } from "react";
+import "./Counter.css";
 
 function Counter() {
-    const [count,setCount]= useState(0);
+  const [count, setCount] = useState(0);
 
-    return(
-        <div>
-            <h1 style={{margin: "50px", padding: "100px 200px"}}>
-                React Counter: {count}
-            </h1>
-            
-            {/* btn to increase */}
-            <button
-                onClick={() => setCount(count+1)}
-                style={{margin: "50px", padding: "10px 20px"}}
-                >
-                 ➕Increase
-            </button>
+  return (
+    <div>
+      <h1 style={{ margin: "50px", padding: "100px 200px" }}>
+        React Counter: {count}
+      </h1>
 
+      {/* btn to increase */}
+      <button
+        onClick={() => setCount(count + 1)}
+        style={{ margin: "50px", padding: "10px 20px" }}
+      >
+        ➕Increase
+      </button>
 
-            {/* btn to decrease */}
-            <button onClick={() => setCount(count-1)} 
-            style={{margin: "50px", padding: "10px 20px"}} >
-                ➖Decrease
+      {/* btn to decrease */}
+      {count > 0 && (
+        <button
+          onClick={() => setCount(count - 1)}
+          style={{ margin: "50px", padding: "10px 20px" }}
+        >
+          ➖Decrease
+        </button>
+      )}
 
-            </button>
-
-            {/* reset button */}
-            <button onClick={() => setCount(0)}
-            style={{margin:"5px", padding:"10px 20px"}}
-            >
-            🔁Reset
-            </button>
-
-
-
-
-
-
-        </div>
-    )
+       {/* reset button  */}
+      {count > 0 && (
+        <button
+          onClick={() => setCount(0)}
+          style={{ margin: "5px", padding: "10px 20px" }}
+        >
+          🔁Reset
+        </button>
+      )}
+    </div>
+  );
 }
 
 export default Counter;
