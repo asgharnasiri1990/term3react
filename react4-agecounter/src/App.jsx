e2ba306a65ddb633e11b46474f103faf089f4b1a
asghar <asgharnasiri1990@gmail.com>
2025-11-18 01:00:09 +0330

    Update App.jsx

--- react4-agecounter/src/App.jsx
+++ react4-agecounter/src/App.jsx
@@ -8,13 +8,13 @@ function Counter() {
     return (
 
         <div>
-            {count > 0 ? <h1 style={{ marginLeft: "190px" }}> {count}</h1> : <h1 style={{ marginLeft: "135px" }}> {count}</h1>}
+            {count > 0 ? <h1 style={{ marginLeft: "220px" }}> {count}</h1> : <h1 style={{ marginLeft: "155px" }}> {count}</h1>}
 
-            <button onClick={() => { setCount(count + 1) }}>Increase Age + </button>
+            <button style={{ color: "green" }} onClick={() => { setCount(count + 1) }}>Increase likes + {count} </button>
 
-            {count > 0 && <button onClick={() => { setCount(0) }}>Reset Age</button>}
+            {count > 0 && <button onClick={() => { setCount(0) }}>Reset likes</button>}
 
-            {count > 0 ? <button onClick={() => { setCount(count - 1) }}>Decrease Age - </button> : <button>Decrease Age -</button>}
+            {count > 0 ? <button style={{ color: "red" }} onClick={() => { setCount(count - 1) }}>Decrease likes - {count} </button> : <button>Decrease Age -</button>}
 
         </div >
 
@@ -24,3 +24,5 @@ function Counter() {
 }
 
 export default Counter;
+
+{toggle=true && <h1></h1> }
