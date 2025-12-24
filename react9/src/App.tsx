048a3a2b483954e601fb7585ab77aa7759f28727
asghar <asgharnasiri1990@gmail.com>
2025-12-25 01:38:08 +0330

    react9

--- react9/src/App.tsx
+++ react9/src/App.tsx
@@ -1,32 +1,45 @@
-import ListGroup from "./components/ListGroup";
+import { useState } from "react"
+import Alert from "./components/Alert"
+import Button from "./components/button"
 
 function App() {
- let items = ['NYC', 'LA', 'Tokyo', 'London', 'Tehran']
-const handleSelectItem=(item:string)=>{
-  console.log(item);
-}
-
+const [alertVisible,setAlertVisibility]=useState(false)
   return (
-  <div> 
-    <ListGroup items={items} heading="cities :" onSelectItem={handleSelectItem} />
-    </div>)
-
-}
-
-
-
-
-import Alert from './components/Alert'
-
-const App = () => {
-  return (
-    <div className='alert alert-primary'>
-      <Alert> 
-        <h2>Hello World!</h2>
-      </Alert>
-
+    <div>
+      {alertVisible && <Alert onClose={()=>setAlertVisibility(false)}>My Alert</Alert>}
+      <Button color="primary" onClick={()=> setAlertVisibility(true) }> My Button</Button>
     </div>
   )
 }
-
 export default App
+// import ListGroup from "./components/ListGroup";
+
+// function App() {
+//  let items = ['NYC', 'LA', 'Tokyo', 'London', 'Tehran']
+// const handleSelectItem=(item:string)=>{
+//   console.log(item);
+// }
+
+//   return (
+//   <div> 
+//     <ListGroup items={items} heading="cities :" onSelectItem={handleSelectItem} />
+//     </div>)
+
+// }
+
+
+
+
+// import Alert from './components/Alert'
+
+// const App = () => {
+//   return (
+//     <div className='alert alert-primary'>
+//       <Alert> 
+//         <h2>Hello World!</h2>
+//       </Alert>
+
+//     </div>
+//   )
+// }
+
--- react9/src/components/Alert.tsx
+++ react9/src/components/Alert.tsx
@@ -2,12 +2,22 @@ import { ReactNode } from "react";
 
 interface Props {
     children: ReactNode;
+    onClose: ()=> void;
 }
 
-const Alert = ({ children }: Props) => {
+const Alert = ({ children,onClose }: Props) => {
     return (
-        <div>{children}</div>
+        <div className="alert alert-primary alert-dismissible">
+            {children}
+
+            <button type="button" className="btn-close"
+             data-bs-dismiss="alert" aria-label="Close"
+             onClick={onClose}
+             >
+
+            </button>
+        </div>
     )
 }
 
-export default Alert
+export default Alert; 
