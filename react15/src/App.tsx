fec8fb43317857a7a7bcc268b59d481c66bd5542
asghar <asgharnasiri1990@gmail.com>
2026-01-08 00:37:05 +0330

    react15

---
+++ react15/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function App() {
+
+  // const [task, setTask] = useState([tasks, value])
+
+  // const [value, setValue] = useState("")
+
+  return (
+    <>
+
+      <input placeholder="enter a task" />
+<div>
+
+      <button >submit</button>
+</div>
+
+      <h1>Tasks:</h1>
+
+    </>
+  )
+};
+
+export default App;
