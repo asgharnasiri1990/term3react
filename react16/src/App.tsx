9cf1616c9c0cff6432e01573864e33b06b8a48ff
asghar <asgharnasiri1990@gmail.com>
2026-02-03 00:28:37 +0330

    react16 resuming

--- react16/src/App.tsx
+++ react16/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState("");
+  const [text, setText] = useState("");
+
+const handleSubmit=()=>{
+  if (!input ) return;
+  setText(input);
+  setInput("")
+}
 
   return (
+
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <input value={input} onChange={(e) => setInput(e.target.value)} placeholder="type something" />
+      <button onClick={handleSubmit}> Submit</button>
+
+      <p>
+        {text}
       </p>
+
     </>
   )
 }
 
-export default App
+export default App;
